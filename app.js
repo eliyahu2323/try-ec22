2614202b8b03f1254eb1d109122bad2b2989fa9f
eliyahu2323 <57942113+eliyahu2323@users.noreply.github.com>
2023-02-18 14:59:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 //Set security HTTP headers
 app.use(helmet());
 
-app.listen(80, () => {
-  console.log(`App running on port ${80}...`);
+app.listen(8080, () => {
+  console.log(`App running on port ${8080}...`);
 });
 
 app.get("/", (req, res) => res.send({ message: "Hello EC2" }));
